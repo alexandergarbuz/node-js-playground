2b3ceacd568a7ca64fc442058fedc8b01f595de4
Alexander Garbuz <alexander_garbuz@homedepot.com>
2025-05-09 07:53:27 -0500

    Loading config from .env file

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const { createServer } = require('node:http');
 require('dotenv').config();
-const hostname = process.env.HOST;
-const port = process.env.PORT;
+const hostname = process.env.HOST || '127.0.0.1';
+const port = process.env.PORT || 3000;
 const server = createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
